4ab7b185745da2feb9e29c4b63e33429da77f200
OmerHai <omer16493@gmail.com>
2020-09-17 13:41:43 +0300

    Finish the checkout page and the cart reducer

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -5,7 +5,7 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
     const existingCartItem = cartItems.find(//Check if item is already exist
         cartItem => cartItem.id === cartItemToAdd.id
     );
-    if(existingCartItem){ //If ys it is return the previous array with the new quantity
+    if(existingCartItem){ //If yes it is return the previous array with the new quantity
         return cartItems.map(//Adds one to the specific cart item quantity
             cartItem => cartItem.id === cartItemToAdd.id
             ? { ...cartItem, quantity: cartItem.quantity + 1}
@@ -14,4 +14,20 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
     }
 
     return [ ...cartItems, { ...cartItemToAdd, quantity:1 }]; // If no it is return the precious array with the new item when the quntity is one
+}
+
+/* This function remove item from cart */
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+    const existingCartItem = cartItems.find(//Check if item is already exist
+        cartItem => cartItem.id === cartItemToRemove.id
+    );
+    if(existingCartItem.quantity === 1){
+        return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id)
+    } 
+    
+    return cartItems.map(//Adds one to the specific cart item quantity
+        cartItem => cartItem.id === cartItemToRemove.id
+        ? { ...cartItem, quantity: cartItem.quantity - 1}
+        : cartItem
+    );
 }
